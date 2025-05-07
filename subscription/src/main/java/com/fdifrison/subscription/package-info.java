160d6a7c83640b933689ac688d1d6c702957a995
fdifrison <ing.giovanni.frison@gmail.com>
2025-05-07 14:44:49 +0200

    cleaning

--- subscription/src/main/java/com/fdifrison/subscription/package-info.java
+++
@@ -1,6 +0,0 @@
-@ApplicationModule(displayName = "user",
-        allowedDependencies = {"common.subscription.dto"}
-)
-package com.fdifrison.subscription;
-
-import org.springframework.modulith.ApplicationModule;
